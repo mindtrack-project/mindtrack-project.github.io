c7cca4e7415435b00592e806534481c9638c4516
Alejandro De León Languré <106360071+langure@users.noreply.github.com>
2025-06-04 15:16:56 -0600

    chore: add PostCSS and Tailwind CSS configuration files

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,72 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import PrimeVue from 'primevue/config'
+import Nora from '@primeuix/themes/nora'
+import { definePreset } from '@primeuix/themes'
+
+import Button from 'primevue/button'
 
 const app = createApp(App)
 
+const MindTrackTheme = definePreset(Nora, {
+  semantic: {
+    primary: {
+      50: '{zinc.50}',
+      100: '{zinc.100}',
+      200: '{zinc.200}',
+      300: '{zinc.300}',
+      400: '{zinc.400}',
+      500: '{zinc.500}',
+      600: '{zinc.600}',
+      700: '{zinc.700}',
+      800: '{zinc.800}',
+      900: '{zinc.900}',
+      950: '{zinc.950}',
+    },
+    colorScheme: {
+      light: {
+        primary: {
+          color: '{zinc.950}',
+          inverseColor: '#ffffff',
+          hoverColor: '{zinc.900}',
+          activeColor: '{zinc.800}',
+        },
+        highlight: {
+          background: '{zinc.950}',
+          focusBackground: '{zinc.700}',
+          color: '#ffffff',
+          focusColor: '#ffffff',
+        },
+      },
+      dark: {
+        primary: {
+          color: '{zinc.50}',
+          inverseColor: '{zinc.950}',
+          hoverColor: '{zinc.100}',
+          activeColor: '{zinc.200}',
+        },
+        highlight: {
+          background: 'rgba(250, 250, 250, .16)',
+          focusBackground: 'rgba(250, 250, 250, .24)',
+          color: 'rgba(255,255,255,.87)',
+          focusColor: 'rgba(255,255,255,.87)',
+        },
+      },
+    },
+  },
+})
+
 app.use(router)
+app.use(PrimeVue, {
+  theme: {
+    preset: MindTrackTheme,
+    options: {
+      darkModeSelector: false || 'none',
+    },
+  },
+})
+
+app.component('Button', Button)
 
 app.mount('#app')
